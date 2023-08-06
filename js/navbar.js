7001536ec8dd450de0e2b37b2014dd9a65591970
Martin <84947017+mngoh@users.noreply.github.com>
2023-08-06 17:12:37 -0400

    updated nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   const toggleButton = document.getElementsByClassName('toggle-button')[0];
   const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-  toggleButton.addEventListener('click', () => {
+
+  toggleButton.addEventListener('click', (event) => {
+    event.preventDefault(); 
     navbarLinks.classList.toggle('active');
   });
-});
 
+  const links = navbarLinks.getElementsByTagName('a');
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', () => {
+      navbarLinks.classList.remove('active');
+    });
+  }
+});
