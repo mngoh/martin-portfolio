f868605fad761e7f16f987db800d6f94edaa6719
Martin <84947017+mngoh@users.noreply.github.com>
2023-08-07 20:31:06 -0400

    updates

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,20 @@
+// Scrolling Navbar Background
 document.addEventListener("DOMContentLoaded", function() {
-  const toggleButton = document.getElementsByClassName('toggle-button')[0];
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+    const toggleButton = document.getElementsByClassName('toggle-button')[0];
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+    const navbar = document.querySelector('.navbar');
 
-  toggleButton.addEventListener('click', (event) => {
-    event.preventDefault(); 
-    navbarLinks.classList.toggle('active');
-  });
-
-  const links = navbarLinks.getElementsByTagName('a');
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', () => {
-      navbarLinks.classList.remove('active');
+    toggleButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      navbarLinks.classList.toggle('active');
+      navbar.classList.toggle('black-background');
     });
-  }
-});
+
+    const links = navbarLinks.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', () => {
+        navbarLinks.classList.remove('active');
+        navbar.classList.remove('black-background');
+      });
+    }
+  });
