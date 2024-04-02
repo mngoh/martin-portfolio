7a89e9dc6b8352ae193fc0f205d8f75e8b5e1f9e
Martin <84947017+mngoh@users.noreply.github.com>
2024-04-02 14:28:47 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -102,3 +102,17 @@ document.addEventListener("DOMContentLoaded", function() {
 //   }
 //   lastScrollY = window.scrollY;
 // });
+
+// Scroll Appear in Website
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+});
+const hiddenElements = document.querySelectorAll('.section-background');
+hiddenElements.forEach((el) => observer.observe(el));
